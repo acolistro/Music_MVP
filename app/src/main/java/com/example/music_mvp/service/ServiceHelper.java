42689d0144dd77287a0a101f3a766fbaee0bad8e
arcolistro <arcolistro@gmail.com>
2020-07-14 04:33:18 -0700

    add servicehelper retrofit

---
+++ app/src/main/java/com/example/music_mvp/service/ServiceHelper.java
@@ -0,0 +1,28 @@
+package com.example.music_mvp.service;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ServiceHelper {
+
+    public static final String BASE_URL = "https://itunes.apple.com/search";
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient() {
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .client(client)
+                    .build();
+        }
+        return retrofit;
+    }
+}
